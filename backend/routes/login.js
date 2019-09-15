395b59c89f90f6733bd6cf5c064a0d2ce5c5b66a
Greg Roques <greg.roques@gmail.com>
2019-09-15 11:14:12 -0400

    fixing backend things

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,34 +4,35 @@ const db = require("../util/database");
 const bcrypt = require("bcrypt");
 const randToken = require("rand-token");
 
-router.post("/login", (req, res, next) => {
+router.post("/login", (req, res) => {
     const { email, password } = req.body;
     const emailFormat = email.toLowerCase();
-    const userQuery = `Select userId, email, hash FROM users WHERE email="${emailFormat}`;
-    db.execute(userQuery).then(res => {
-        if (res.length === 0) {
+    const userQuery = `Select email, userId, hash FROM users WHERE email="${emailFormat}"`;
+    db.execute(userQuery).then(results => {
+        if (results[0].length === 0) {
             res.json({
                 msg: "Email Invalid"
             });
         } else {
-            const { userId, email, hash } = res[0];
+            const { userId, email, hash } = results[0][0];
             const checkHash = bcrypt.compareSync(password, hash);
             if (checkHash) {
                 const token = randToken.uid(50);
-                const updateUserToken = `UPDATE users SET idToken="${token}" WHERE email="${email}`;
-                db.execute(updateUserToken).then(() =>
-                    res.json({
-                        userId,
-                        email,
-                        idtoken: token
-                    })
-                ).catch(err => { throw err; });
+                const updateUserToken = `UPDATE users SET idToken="${token}" WHERE email="${email}"`;
+                db.execute(updateUserToken).catch(err2 => { throw err2; });
+                res.json({
+                    userId,
+                    email,
+                    idtoken: token
+                });
             } else {
                 res.json({
                     msg: "Password Invalid"
                 });
-            };
+            }
         }
+    }).catch(err => {
+        throw err;
     });
 });
 
--- src/Pages/Admin/Login.js
+++ src/Pages/Admin/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import * as actions from "../../Redux/Reducers/Auth";
+import { connect } from "react-redux";
+import * as actions from "../../Redux/Actions/Auth";
 import cssLogin from "./AdminCSS/login.module.css";
 
 class Login extends Component {
@@ -17,8 +17,8 @@ class Login extends Component {
 
     onSubmitHandler = e => {
         e.preventDefault();
-        // const { email, password } = this.state;
-        // this.props.onAuth(email, password);
+        const { email, password } = this.state;
+        this.props.onAuth(email, password);
     };
 
     render () {
@@ -51,12 +51,12 @@ class Login extends Component {
     }
 };
 
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         onAuth: (email, password) => dispatch(actions.auth(email, password))
-//     };
-// };
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
 
-// export default connect(null, mapDispatchToProps)(Login);
+export default connect(null, mapDispatchToProps)(Login);
 
-export default Login;
+// export default Login;
