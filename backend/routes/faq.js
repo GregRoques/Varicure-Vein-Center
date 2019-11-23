1576bf842546b98384cff2c463e70bbe448ec686
Greg Roques <greg.roques@gmail.com>
2019-11-23 14:02:53 -0500

    howdy

--- backend/routes/faq.js
+++ backend/routes/faq.js
@@ -15,64 +15,64 @@ router.get("/faq/:hash", (req, res) => {
         });
 });
 
-router.post("/faq/addfaq", (req, res) => {
-    const { language, question, answer } = req.query;
-    const addFAQ = `INSERT INTO faq (question, answer, language) VALUES ('${question}', '${answer}', '${language}')`;
-    db.execute(addFAQ).then(() => {
-        const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
-        db.execute(selectFAQ).then(results => {
-            const myFaqs = results[0];
-            res.json(myFaqs);
-        }).catch(err => {
-            if (err) {
-                throw err;
-            }
-        });
-    }).catch(err => {
-        if (err) {
-            throw err;
-        }
-    });
-});
+// router.post("/faq/addfaq", (req, res) => {
+//     const { language, question, answer } = req.query;
+//     const addFAQ = `INSERT INTO faq (question, answer, language) VALUES ('${question}', '${answer}', '${language}')`;
+//     db.execute(addFAQ).then(() => {
+//         const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
+//         db.execute(selectFAQ).then(results => {
+//             const myFaqs = results[0];
+//             res.json(myFaqs);
+//         }).catch(err => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     }).catch(err => {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+// });
 
-router.post("/faq/updatefaq", (req, res) => {
-    const { id, language, question, answer } = req.query;
-    const updateFAQ = `UPDATE faq SET question="${question}", answer="${answer}" WHERE id="${id}"`;
-    db.execute(updateFAQ).then(() => {
-        const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
-        db.execute(selectFAQ).then(results => {
-            const myFaqs = results[0];
-            res.json(myFaqs);
-        }).catch(err => {
-            if (err) {
-                throw err;
-            }
-        });
-    }).catch(err => {
-        if (err) {
-            throw err;
-        }
-    });
-});
+// router.post("/faq/updatefaq", (req, res) => {
+//     const { id, language, question, answer } = req.query;
+//     const updateFAQ = `UPDATE faq SET question="${question}", answer="${answer}" WHERE id="${id}"`;
+//     db.execute(updateFAQ).then(() => {
+//         const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
+//         db.execute(selectFAQ).then(results => {
+//             const myFaqs = results[0];
+//             res.json(myFaqs);
+//         }).catch(err => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     }).catch(err => {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+// });
 
-router.post("/faq/deletefaq", (req, res) => {
-    const { id, language } = req.query;
-    const deleteFAQ = `DELETE * FROM faq WHERE id="${id}"`;
-    db.execute(deleteFAQ).then(() => {
-        const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
-        db.execute(selectFAQ).then(results => {
-            const myFaqs = results[0];
-            res.json(myFaqs);
-        }).catch(err => {
-            if (err) {
-                throw err;
-            }
-        });
-    }).catch(err => {
-        if (err) {
-            throw err;
-        }
-    });
-});
+// router.post("/faq/deletefaq", (req, res) => {
+//     const { id, language } = req.query;
+//     const deleteFAQ = `DELETE * FROM faq WHERE id="${id}"`;
+//     db.execute(deleteFAQ).then(() => {
+//         const selectFAQ = `SELECT id, question FROM faq WHERE language='${language}'`;
+//         db.execute(selectFAQ).then(results => {
+//             const myFaqs = results[0];
+//             res.json(myFaqs);
+//         }).catch(err => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     }).catch(err => {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+// });
 
 module.exports = router;
--- backend/routes/staff.js
+++ backend/routes/staff.js
@@ -15,24 +15,24 @@ router.get("/staff/:hash", (req, res) => {
         });
 });
 
-router.post("/staff/updatestaff", (req, res) => {
-    const { name, title, bio, language } = req.query;
-    const updateStaff = `UPDATE staff SET name="${name}", title="${title}", bio="${bio}" WHERE language="${language}"`;
-    db.execute(updateStaff).then(() => {
-        const selectStaff = `SELECT name, title, bio FROM staff WHERE language='${language}'`;
-        db.execute(selectStaff).then(results => {
-            const myStaff = results[0][0];
-            res.json(myStaff);
-        }).catch(err => {
-            if (err) {
-                throw err;
-            }
-        });
-    }).catch(err => {
-        if (err) {
-            throw err;
-        }
-    });
-});
+// router.post("/staff/updatestaff", (req, res) => {
+//     const { name, title, bio, language } = req.query;
+//     const updateStaff = `UPDATE staff SET name="${name}", title="${title}", bio="${bio}" WHERE language="${language}"`;
+//     db.execute(updateStaff).then(() => {
+//         const selectStaff = `SELECT name, title, bio FROM staff WHERE language='${language}'`;
+//         db.execute(selectStaff).then(results => {
+//             const myStaff = results[0][0];
+//             res.json(myStaff);
+//         }).catch(err => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     }).catch(err => {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+// });
 
 module.exports = router;
