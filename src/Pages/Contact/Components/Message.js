9a651db29bb8c2e51682095ba39bb62a9719f92d
Greg Roques <greg.roques@gmail.com>
2019-11-28 09:43:40 -0500

    front-end js complete

--- src/Pages/Contact/Components/Message.js
+++ src/Pages/Contact/Components/Message.js
@@ -50,28 +50,35 @@ class Message extends Component {
         return (
             <form className={ cssMessage.messageGrid } onChange={ this.onChangeHandler } onSubmit={e => this.onSubmitHanlder(e)} >
                 <div>
-                    <div className={ cssMessage.header }>Message</div>
-                    <div className={ cssMessage.intro }>Send a brief message to our lead physician and we will get back to you within two business days.</div>
-                    <input className={ cssMessage.shortForm } type="text" name="name" placeholder="FULL NAME" value={ this.state.name } required/> <br/>
-                    <input className={ cssMessage.shortForm } type="email" name="email" placeholder="EMAIL" value={ this.state.email } required/> <br/>
-                    <input className={ cssMessage.shortForm } type="tel" maxLength="14" name="phone" placeholder="PHONE (Optional)" value={ this.state.phone }/> <br/>
+                    <div className={ cssMessage.header }>
+                        { this.props.isEnglish === "e" ? "Message" : "Mensaje" }
+                    </div>
+                    <div className={ cssMessage.intro }>
+                        { this.props.isEnglish === "e"
+                            ? "Send a brief message to our lead physician and we will get back to you within two business days."
+                            : "Envíe un breve mensaje a nuestro médico principal y le responderemos dentro de dos días hábiles."
+                        }
+                    </div>
+                    <input className={ cssMessage.shortForm } type="text" name="name" placeholder={ this.props.isEnglish === "e" ? "Full Name" : "Nombre Completo" } value={ this.state.name } required/> <br/>
+                    <input className={ cssMessage.shortForm } type="email" name="email" placeholder={ this.props.isEnglish === "e" ? "Email" : "Correo Electrónico" } value={ this.state.email } required/> <br/>
+                    <input className={ cssMessage.shortForm } type="tel" maxLength="14" name="phone" placeholder={ this.props.isEnglish === "e" ? "Phone (Optional)" : "Telefono (Opcion)" } value={ this.state.phone }/> <br/>
                     <select className={ cssMessage.shortForm } name="services" required>
-                        <option value="" disabled selected >SERVICE LINE</option>
+                        <option value="" disabled selected >{ this.props.isEnglish === "e" ? "SERVICES" : "SERVICIOUS" }</option>
                         <option value="Kill Spider Man">Kill Spider Man</option>
                         <option value="laser hair removal">Laser Hair Removal</option>
                         <option value="cybernetic implants">Cybernetic Limbs </option>
                         <option value="laser eyes"> Laser Eyes </option>
-                        <option value="General Service Questions"> General Questions/Other </option>
+                        <option value="General Service Questions">{ this.props.isEnglish === "e" ? "Other" : "Otra" }</option>
                     </select>
                     <div className={ cssMessage.radioBoxes }>
-                        Preferred Contact:
-                        <input className={ cssMessage.radio } type="radio" name="contact" value="email" checked={this.state.contact === "email"}/> Email
-                        <input className={ cssMessage.radio } type="radio" name="contact" value="phone"/> Phone
+                        { this.props.isEnglish === "e" ? "Preferred Contact:" : "Contacto Preferido:" }
+                        <input className={ cssMessage.radio } type="radio" name="contact" value="email" checked={this.state.contact === "email"}/> { this.props.isEnglish === "e" ? "Email" : "Correo Electrónico" }
+                        <input className={ cssMessage.radio } type="radio" name="contact" value="phone"/> { this.props.isEnglish === "e" ? "Phone" : "Telefono" }
                     </div>
                 </div>
                 <div>
-                    <textarea className={ cssMessage.messageForm} type="text" name="message" placeholder="MESSAGE" value={ this.state.message } required/>
-                    <button type="submit" className={ cssMessage.submit }>SUBMIT</button>
+                    <textarea className={ cssMessage.messageForm} type="text" name="message" placeholder={ this.props.isEnglish === "e" ? "Message" : "Mensaje" } value={ this.state.message } required/>
+                    <button type="submit" className={ cssMessage.submit }>{ this.props.isEnglish === "e" ? "SUBMIT" : "ENVIAR" }</button>
                 </div>
             </form>
         );
