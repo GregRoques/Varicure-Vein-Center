838037bec189156825b1bb50422d9523b06a594b
Greg Roques <greg.roques@gmail.com>
2020-01-05 14:58:17 -0500

    bcrypt fix

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../util/database");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-nodejs");
 const randToken = require("rand-token");
 
 router.post("/login", (req, res) => {
