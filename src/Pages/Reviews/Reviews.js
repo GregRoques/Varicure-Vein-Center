b855ad8c7d8ad1ac7a45cfa87ebfab9cd02eb738
Greg Roques <greg.roques@gmail.com>
2020-02-14 08:41:36 -0500

    update

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -13,8 +13,19 @@ const Reviews = props => {
                 We are very grateful to have you as a patient and hope your experience with us was pleasant and effective.
                 Please consider writing a review using one of the links below and let future visitors know what to expect from their treatment.
             </div>
-            <div className={cssReviews.shareHeader}>Write a Review</div>
-            <div className={cssReviews.shareHeader}>Share with a Friend</div>
+            <div className={ cssReviews.reviewButtonContainer }>
+                <span >
+                    <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2FvaricureMiami%2Freviews%2F" target="_blank" rel="noopener noreferrer">
+                        <img className={ cssReviews.fbReview } src="/reviewImages/fbReview.jpg" alt="Facebook Review"/>
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank" rel="noopener noreferrer" href="https://g.page/VaricureMiami/review?rc" >
+                        <img className={ cssReviews.gglReview } src="/reviewImages/gglReview.png" alt="Google Review"/>
+                    </a>
+                </span>
+            </div>
+            <hr className={ cssReviews.reviewSeperator } align="center" width="65%"/>
         </div>
     );
 };
@@ -26,7 +37,3 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, null)(Reviews);
-
-// https://g.page/VaricureMiami/review?rc
-
-// http://www.facebook.com/varicureMiami/reviews
