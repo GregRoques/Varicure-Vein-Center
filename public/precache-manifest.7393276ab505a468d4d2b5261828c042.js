944338f996d0e577cb13dc7354efac40aac663e2
Greg Roques <greg.roques@gmail.com>
2020-02-16 13:42:34 -0500

    update

---
+++ public/precache-manifest.7393276ab505a468d4d2b5261828c042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840c6c80e7a25709a93cc1027a347efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "befac059c86307659654",
+    "url": "/static/css/main.9a559a41.chunk.css"
+  },
+  {
+    "revision": "3e97f4f0e44993281b1f",
+    "url": "/static/js/2.ecbc9432.chunk.js"
+  },
+  {
+    "revision": "09a486f123bb1c50891011b034b7d369",
+    "url": "/static/js/2.ecbc9432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befac059c86307659654",
+    "url": "/static/js/main.d88fc3b8.chunk.js"
+  },
+  {
+    "revision": "148370fed59b500f88f8",
+    "url": "/static/js/runtime-main.d8e9333d.js"
+  }
+]);
