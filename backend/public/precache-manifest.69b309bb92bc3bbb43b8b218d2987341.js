ff9c803249749bf12bfe6f19651698b673979980
Greg Roques <greg.roques@gmail.com>
2020-03-22 18:28:08 -0400

    corona popup build

---
+++ backend/public/precache-manifest.69b309bb92bc3bbb43b8b218d2987341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb99c3226d3ee1919a4cf7ddf1d0e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4be39816081ca315d50",
+    "url": "/static/css/main.63eeab3a.chunk.css"
+  },
+  {
+    "revision": "511e4d8d4dbdb3df6020",
+    "url": "/static/js/2.f4b58b0d.chunk.js"
+  },
+  {
+    "revision": "79fe4468422467fae69ae3ccfcb1c401",
+    "url": "/static/js/2.f4b58b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4be39816081ca315d50",
+    "url": "/static/js/main.a5e46274.chunk.js"
+  },
+  {
+    "revision": "bb1c15a73128ccae1934",
+    "url": "/static/js/runtime-main.763ed6b8.js"
+  }
+]);
--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60bff9854ce19cc72cd3c97d42872c83.js"
+  "/precache-manifest.69b309bb92bc3bbb43b8b218d2987341.js"
 );
 
 self.addEventListener('message', (event) => {
