2fe525ff78bf50882150258e79b909450b68f9af
Greg Roques <greg.roques@gmail.com>
2020-06-24 18:16:47 -0400

    update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -14,14 +14,14 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const myBackend = "/backend/";
-app.use(`${myBackend}instaImages`, instaImages);
-app.use(`${myBackend}personalData`, myContacts);
+const myBackend = "/backend";
+app.use(`${myBackend}/instaImages`, instaImages);
+app.use(`${myBackend}/personalData`, myContacts);
 
-// app.use(express.static(path.join(__dirname, "public")));
-// app.get("/*", (req, res, next) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "public")));
+app.get(`/*`, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = 2000;
 app.listen(PORT, () => {
